8839b23012dd7ba97868e68d3a940174e7dd5eae
jrbeverly <jrbeverly@live.com>
2017-06-04 19:21:47 -0400

    Pushing vagrant-desktop changes

--- src/packaging/environments/ubuntu-minimal.sh
+++ src/packaging/environments/ubuntu-minimal.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 start="$(date +%s)"
+logfile=/vagrant/vagrant.log
+
 echo "Provisioning Desktop Environment"
 echo "-----------------------------"
 
-apt-get -y install --no-install-recommends ubuntu-desktop 
-apt-get -y install --install-recommends unity indicator-session
-apt-get -y install gnome-terminal
+echo "Installing the minimal ubuntu-desktop, this may take a little bit."
+apt-get -y install --no-install-recommends ubuntu-desktop >>$logfile 2>&1
+apt-get -y install --install-recommends unity indicator-session >>$logfile 2>&1
+apt-get -y install gnome-terminal >>$logfile 2>&1
 
 end="$(date +%s)"
 echo "-----------------------------"
-echo "Provisioning complete in $(($end - $start)) seconds"
+echo "The provisioning step completed in $(($end - $start)) seconds"
